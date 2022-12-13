770c816d60a5a0c80c212edf7f5c82fe16dadfb2
Lars Teeffelen <lars.teeffelen@gmail.com>
2022-12-13 22:56:38 +0100

    Initial commit

---
+++ src/main/java/hammynl/rebootly/Rebootly.java
@@ -0,0 +1,17 @@
+package hammynl.rebootly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Rebootly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
